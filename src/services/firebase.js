9086cdd1a431f7dba22085c9b74c020685151b22
Romadhon Aji <60551839+romaaji@users.noreply.github.com>
2022-04-29 14:29:36 +0700

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth'
 
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyDPgmkBf2qCiWivIybjDbD_RUcnKOwkWcw",
-    authDomain: "endlesstv.firebaseapp.com",
-    databaseURL: "https://endlesstv-default-rtdb.firebaseio.com",
-    projectId: "endlesstv",
-    storageBucket: "endlesstv.appspot.com",
-    messagingSenderId: "673035889001",
-    appId: "1:673035889001:web:ffb3befcde496b4ba4b5f8"
+    apiKey: "YOUR FIREBASE APP",
+    authDomain: "YOUR FIREBASE APP",
+    databaseURL: "YOUR FIREBASE APP",
+    projectId: "YOUR FIREBASE APP",
+    storageBucket: "YOUR FIREBASE APP",
+    messagingSenderId: "YOUR FIREBASE APP",
+    appId: "YOUR FIREBASE APP"
 })
 
 export const auth = app.auth();
